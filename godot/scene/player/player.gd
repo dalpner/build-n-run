ad083ba2058ed737f869fa56bf4ae622779743d5
leipneralvarez_dom <dominicleipner@gmx.de>
2023-10-27 12:05:49 +0200

    rotation on jump

--- godot/scene/player/player.gd
+++ godot/scene/player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
+var jump_direction = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -18,14 +19,22 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		rotate(abs(velocity.x) / 420 * jump_direction)
+	
+	if  is_on_floor():
+		rotation = 0
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		print(direction)
+		jump_direction = direction
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
 
 	if direction == -1:
 		sprite.set_flip_h(false);
@@ -39,9 +48,9 @@ func _physics_process(delta):
 	else:
 		if velocity.y != 0:
 			anim.play("Jump")
-			rotate(velocity.x)
 		if velocity.y == 0:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			anim.play("Idle")
+			
 
 	move_and_slide()
